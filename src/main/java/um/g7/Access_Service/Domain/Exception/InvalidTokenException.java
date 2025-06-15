e0ce24a2566f8995ad9d8f0fef770a4da809e32d
spereira04 <santipereira1987@gmail.com>
2025-06-14 21:45:39 -0300

    fixed error message when invalid jwt

--- src/main/java/um/g7/Access_Service/Domain/Exception/InvalidTokenException.java
+++ src/main/java/um/g7/Access_Service/Domain/Exception/InvalidTokenException.java
@@ -3,7 +3,7 @@ package um.g7.Access_Service.Domain.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.PAYMENT_REQUIRED)
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
 public class InvalidTokenException extends RuntimeException{
     public InvalidTokenException(String err) {
         super(err);
